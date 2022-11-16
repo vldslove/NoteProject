c882967168c2dd24cec9390e6403ff0a03967517
vldslove <gsmgodmod@gmail.com>
2022-11-16 22:57:23 +0300

    выполнил дз 20

--- app/src/main/java/com/example/an10_onl/NoteListViewHolder.kt
+++ app/src/main/java/com/example/an10_onl/NoteListViewHolder.kt
@@ -1,6 +1,8 @@
 package com.example.an10_onl
 
+import android.content.Intent
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,7 +12,17 @@ class NoteListViewHolder(private val view: View) : RecyclerView.ViewHolder(view)
             findViewById<TextView>(R.id.title).text = note.title
             findViewById<TextView>(R.id.message).text = note.message
             findViewById<TextView>(R.id.date).text = note.date
+            findViewById<Button>(R.id.shareButton).setOnClickListener{
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_TEXT,  note.message)
+                val chosenIntent = Intent.createChooser(intent, "qwe")
+                context.startActivity(chosenIntent)
+            }
 
         }
+
     }
+
 }
+
